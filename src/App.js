64ddcc3569a996279f06f88abd3392ac8c9757ed
ttran-bread <tiffany.tran@breadfinancial.com>
2023-05-12 14:39:59 -0500

    gallery template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="container text-center mt-5">
-      <h1>Gallery</h1>
-
-    </div>
+    <>
+      <Navbar />
+      <div className="container text-center mt-5">
+        <h1>Gallery</h1>
+        <div className="row">
+          {Array.apply(null, { length: 9 }).map(() => (
+            <Card />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <div className="col mb-5">
+      <div class="card" style={{ width: "18rem" }}>
+        <img
+          src="https://via.placeholder.com/200"
+          class="card-img-top"
+          alt="image"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
